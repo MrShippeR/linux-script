3f25f31437c32201f8b86c8aad286ee45957c28c
Marek Vach <marek@vach.cz>
2021-07-10 11:48:36 +0200

    Druhá verze programu
    Chybí instalační instrukce pro Nextcloud.

--- marvac-install-programs-after-new-os.sh
+++ marvac-install-programs-after-new-os.sh
@@ -12,7 +12,7 @@ menu=(
 "7) instalovat Signal"
 "8) instalovat Docker a Docker-Container"
 "9) instalovat driver tiskárny Samsung M2020"
-"10) instalovat KolorPaint"
+"10) instalovat KolourPaint"
 "11) instalovat net-tools"
 "12) instalovat TeamViewer")
 menu+=([100]="100) ukončit skript")
@@ -20,11 +20,13 @@ menu+=([100]="100) ukončit skript")
 highest_menu_number=$(echo $((${#menu[@]} - 2))) # count of array minus 0 and 100
 do_all_tasks=false
 blue='\033[0;34m'
+green='\033[0;32m'
 orange='\033[0;33m'
+red='\033[0;31m'
 no_color='\033[0m'
 sleep_time=0.8
 
-# define functions #
+# define functions and install instructions #
 do_switch_case() {
 	printf "${orange}Započínám vykonávání úkoly $choice:${no_color}"
 	sleep $sleep_time
@@ -32,7 +34,7 @@ do_switch_case() {
 
 		1)
 			sudo apt-get install gnome-tweaks
-			# gnome-tweaks &  # & starts separate process
+ 			# gnome-tweaks &  # & starts separate process
 		;;
 
 		2)		
@@ -56,31 +58,94 @@ do_switch_case() {
 		;;
 
 		6)
-			
+			sudo snap install discord
 		;;
 
 		7)
-			
+			sudo snap install signal-desktop
 		;;
 
 		8)
+			echo "Instalace potřebných podprogramů a přidání repozitáře..."
+			sleep $sleep_time
 			
+			sudo apt-get update
+			sudo apt-get install \
+    				apt-transport-https \
+    				ca-certificates \
+    				curl \
+    				gnupg \
+    				lsb-release
+			curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+			echo \
+  			"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  			$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+			echo "Instalace docker engine..."
+			sleep $sleep_time
+			sudo apt-get update
+			sudo apt-get install docker-ce docker-ce-cli containerd.io
+			docker version
+
+			echo "Instalace docker-compose..."
+			sleep $sleep_time
+			sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+			sudo chmod +x /usr/local/bin/docker-compose
+			docker-compose --version
+
+			echo "Nastavení automatického spuštění a přidání uživatele do skupiny docker"
+			sleep $sleep_time
+			sudo groupadd docker
+			sudo usermod -aG docker $USER	
+			echo "Pro aktivaci oprávnění je potřeba restartovat PC..."	
+
+			sudo systemctl enable docker.service	
+			sudo systemctl enable containerd.service
 		;;
 
-		9)
+		9)			
+			file_name=samsung-M2020-driver-linux.tar.gz
+			cd ~/Stažené			
 			
+			if test -f "$file_name"; then
+				echo ""				
+				echo "Soubor již stažený je, přeskakuji krok..."
+			else
+				wget http://verejne.marvac.tech/$file_name
+			fi
+
+			if test -d "uld"; then
+				echo "Extrahovaná složka již existuje, přeskakuji extrakci..."
+			else
+				tar xvzf samsung-M2020-driver-linux.tar.gz
+			fi
+
+			cd uld/
+			printf "${red}V EULA je 10 bodů, u bodu 9 zpomalit, pak Y, jinak skript spadne! ${no_color}"
+			sleep 3
+			echo ""
+			sudo ./install.sh
 		;;
 
 		10)
-			
+			sudo snap install kolourpaint
 		;;
 
 		11)
-			
+			sudo apt install net-tools
+			echo ""
+			echo "Otestování nástroje PING:"
+			ping -c 2 google.com
+			echo ""
+			ping -c 2 vach.cz
 		;;
 
 		12)
-			
+			cd ~/Stažené
+			wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+			sudo dpkg -i teamviewer_amd64.deb	
+			rm ~/Stažené/teamviewer_amd64.deb
+			echo "Instalační soubor teamviewer_amd64.deb odstraněn."	
 		;;
 
 		100)
@@ -89,12 +154,14 @@ do_switch_case() {
 
 	esac
 	echo ""
-	printf "${blue}Úloha ${menu[$choice]} dokončena.${no_color}"
+	printf "${green}Úloha ${menu[$choice]} dokončena.${no_color}"
 	echo ""
 	((choice++))
 	sleep $sleep_time
 }
 ######
+
+# Main function - user unput and program logic #
 
 echo ""
 echo "Skript pro rychlou instalaci 2021 Marek@Vach.cz v0.1"
