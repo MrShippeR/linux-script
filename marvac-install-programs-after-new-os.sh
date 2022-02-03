be563876cca992bc493d2447e19911fdb66b5fe5
Marek Vach <marek@vach.cz>
2022-02-03 22:05:49 +0100

    Update marvac-install-programs-after-new-os.sh

--- marvac-install-programs-after-new-os.sh
+++ marvac-install-programs-after-new-os.sh
@@ -52,7 +52,11 @@ do_switch_case() {
 		;;
 
 		3)
-			sudo snap install brave
+			sudo apt install apt-transport-https curl
+			sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+			echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+			sudo apt update
+			sudo apt install brave-browser
 		;;
 
 		4)	
