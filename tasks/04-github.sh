e61a5d8d82d6dfe19e1ffb8b266a72858a31d189
Marek Vach <marek@vach.cz>
2026-01-06 19:36:42 +0100

    Added tasks for CrowdSec.

--- tasks/04-github.sh
+++ tasks/04-github.sh
@@ -7,12 +7,25 @@ git config --global user.email "marek@vach.cz"
 log "Testuji SSH spojení s GitHubem..."
 # Použijeme expect pro automatické 'yes' při první autentizaci
 expect <<'EOF'
+set timeout 20
 spawn ssh -T git@github.com
+
 expect {
     "Are you sure you want to continue connecting (yes/no)?" {
         send "yes\r"
         exp_continue
     }
-    eof
+    "successfully authenticated" {
+        exit 0
+    }
+    "Permission denied" {
+        exit 1
+    }
+    eof {
+        exit 0
+    }
+    timeout {
+        exit 1
+    }
 }
 EOF
